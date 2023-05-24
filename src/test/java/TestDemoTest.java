cedbd6b1bed611e631db9f08fa980a691ad73d68
Cheenue Thao <cheenuethao@gmail.com>
2023-05-24 16:23:56 -0500

    Week 12 coding assignment

--- src/test/java/TestDemoTest.java
+++ src/test/java/TestDemoTest.java
@@ -1,18 +1,21 @@
-import org.junit.jupiter.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
+
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
 
 public class TestDemoTest {
     private TestDemo testDemo;
@@ -26,22 +29,21 @@ public class TestDemoTest {
     @MethodSource ("TestDemoTest#argumentsForAddPositive")
     public void assertThatTwoPositiveNumbersAreAddedCorrectly(int a, int b, int expected, Boolean expectException) {
         if (!expectException) {
-            assertThat(testDemo.addPositive(a, b)).isEqualTo(expected);
+            assertThat(testDemo.addPositive(a, b), is(equalTo(expected)));
         } else {
             assertThatThrownBy(() -> testDemo.addPositive(a, b))
                     .isInstanceOf(IllegalArgumentException.class);
         }
     }
 
+    @NotNull
     private static Stream<Arguments> argumentsForAddPositive() {
         return Stream.of(
-                Arguments.of(2, 3, 5),
-                Arguments.of(0, 5, 0),
-                Arguments.of(10, -2, 0),
-                Arguments.of(7, 8, 15)
+                arguments(2, 4, 6, false)
         );
     }
 
+    @Test
     void assertThatNumberSquaredIsCorrect() {
         TestDemo testDemo = new TestDemo();
         TestDemo mockDemo = spy(testDemo);
@@ -50,7 +52,7 @@ public class TestDemoTest {
 
         int fiveSquared = mockDemo.randomNumberSquared();
 
-        assertThat(fiveSquared).isEqualTo(25);
+        assertThat(fiveSquared, is(equalTo(25)));
     }
 }
 
